0553f17770532cdc92f0e06e6d3647792c9c3558
task.sakamoto <task3028@gmail.com>
2022-07-02 13:39:19 +0000

    revise_authentication

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -85,6 +85,10 @@ class TasksController extends Controller
     {
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
+        
+        if (\Auth::id() === $task->user_id) {
+            $task->get();
+        }
 
         // メッセージ詳細ビューでそれを表示
         return view('tasks.show', [
@@ -102,6 +106,10 @@ class TasksController extends Controller
     {
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
+        
+        if (\Auth::id() === $task->user_id) {
+            $task->post();
+        }
 
         // メッセージ編集ビューでそれを表示
         return view('tasks.edit', [
@@ -126,6 +134,10 @@ class TasksController extends Controller
         
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
+        
+        if (\Auth::id() === $task->user_id) {
+            $task->put();
+        }
         // タスクを更新
         $task->status = $request->status;
         $task->content = $request->content;
